a747d5568b269ae869ce17cabaf296d0b37f8eed
cumtlzy <348841846@qq.com>
2024-06-19 18:41:22 +0800

    mp配置(分页插件)

---
+++ common/service-util/src/main/java/com/atguigu/ssyx/common/config/MybatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.atguigu.ssyx.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * ClassName: MybatisPlusConfig
+ * Package: com.atguigu.ssyx.common.config
+ * desc: mp配置
+ * @author lzyy
+ */
+
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.atguigu.ssyx.*.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * mp plug
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
